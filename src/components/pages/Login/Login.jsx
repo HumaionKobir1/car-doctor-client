db7d301a0bb6184db7a795694fb44ef522d958d3
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-05-11 20:11:40 +0600

    Login page setup and create auth context setup

---
+++ src/components/pages/Login/Login.jsx
@@ -0,0 +1,47 @@
+import img from '../../../assets/images/login/login.svg'
+const Login = () => {
+    const handleLogin = event => {
+        event.preventDefault();
+    }
+    return (
+        <div>
+            
+            <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row">
+                <div className="text-center lg:text-left w-1/2 mr-12">
+                    <img src={img} alt="" />
+                </div>
+
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <h1 className="text-5xl text-center font-bold">Login now!</h1>
+
+                <div className="card-body">
+                    <form onSubmit={handleLogin}>
+                        <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="text" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="text" placeholder="password" className="input input-bordered" />
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <input type="submit" className='btn bg-[#FF3811] border-none' value="LOGIN" />
+                        </div>
+                    </form>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
