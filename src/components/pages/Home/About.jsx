57544ac62b10c80c3a8ef931e91d2d2b489b3f88
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-05-11 13:57:39 +0600

    create about section with image relative position

---
+++ src/components/pages/Home/About.jsx
@@ -0,0 +1,23 @@
+import person from '../../../assets/images/about_us/person.jpg'
+import parts from '../../../assets/images/about_us/parts.jpg'
+const About = () => {
+    return (
+        <div>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row">
+                    <div className='lg:w-1/2 relative'>
+                        <img src={person} className="w-3/4 rounded-lg shadow-2xl" />
+                        <img src={parts} className="w-1/2 absolute right-5 top-1/2 rounded-lg shadow-2xl  border-8 border-white" />
+                    </div>
+                    <div className='lg:w-1/2'>
+                        <h1 className="text-5xl font-bold">Box Office News!</h1>
+                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <button className="btn btn-primary">Get Started</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import About from "./About";
 import Banner from "./Banner";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <About></About>
             <h1>this is Home</h1>
         </div>
     );
