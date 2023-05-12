a41543bc7e169311bf1583042f485502f2b644ee
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-05-12 18:38:10 +0600

    Insert bulk data in database

---
+++ src/components/pages/CheckOut/CheckOut.jsx
@@ -0,0 +1,10 @@
+
+const CheckOut = () => {
+    return (
+        <div>
+            <h1>this is checkout page</h1>
+        </div>
+    );
+};
+
+export default CheckOut;
--- src/components/pages/Home/ServiceCard.jsx
+++ src/components/pages/Home/ServiceCard.jsx
@@ -1,6 +1,7 @@
+import { Link } from "react-router-dom";
 
 const ServiceCard = ({service}) => {
-    const {img, title, price} = service;
+    const {_id, img, title, price} = service;
     return (
         <div className=" shadow-md rounded-md overflow-hidden p-4">
         <img src={img} alt="Placeholder" className="w-full h-44 object-cover rounded-lg" />
@@ -8,11 +9,13 @@ const ServiceCard = ({service}) => {
                 <h3 className="text-xl font-medium mt-2">{title}</h3>
                 <div className="flex items-center justify-between mb-4">
                 <span className="text-base font-medium text text-[#FF3811]">Price: ${price}</span>
-                <button className="flex items-center justify-center w-8 h-8 rounded-full bg-[#FF3811] hover:bg-gray-300 transition-colors duration-300">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-white" viewBox="0 0 20 20" fill="currentColor">
-                    <path fillRule="evenodd" d="M6.293 5.293a1 1 0 011.414 0L12 9.586V7a1 1 0 112 0v4a1 1 0 01-1 1h-4a1 1 0 110-2h2.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" />
-                    </svg>
-                </button>
+                <Link to={`/checkout/${_id}`} >
+                    <button className="flex items-center justify-center w-8 h-8 rounded-full bg-[#FF3811] hover:bg-gray-300 transition-colors duration-300">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-white" viewBox="0 0 20 20" fill="currentColor">
+                        <path fillRule="evenodd" d="M6.293 5.293a1 1 0 011.414 0L12 9.586V7a1 1 0 112 0v4a1 1 0 01-1 1h-4a1 1 0 110-2h2.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" />
+                        </svg>
+                    </button>
+                </Link>
                 </div>
             </div>
         </div>
